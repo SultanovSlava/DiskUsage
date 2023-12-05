e1cf7bca2a36ea644711d9a88b02400f6627c55d
SultanovSlava <sultanoffslav@gmail.com>
2023-12-05 22:53:06 +0500

    norm_level

--- files_walking.py
+++ files_walking.py
@@ -7,26 +7,26 @@ class files_walking:
 
     def walking(self, path):
         self.level_files_and_dirs[path] = []
+        self.cur_level += 1
         try:
             files_number = 0
             files_and_dirs = os.listdir(path)
             for f in files_and_dirs:  # [f for f in files_and_dirs if os.path.isfile(f)]:
                 if os.path.isfile(os.path.join(path, f)):
-                    file = file_info.my_file(os.path.join(path, f), self.cur_level)
+                    file = file_info.my_file(os.path.join(path, f), self.cur_level - 1)
                     # print(file.name)
                     self.level_files_and_dirs[path].append(file)
                     # print(file.size)
                     files_number += 1
                 else:
-
-                    folder = file_info.folder(os.path.join(path, f), self.cur_level)
+                    folder = file_info.folder(os.path.join(path, f), self.cur_level - 1)
                     # print(folder.name)
                     self.level_files_and_dirs[path].append(folder)
                     # print('!!! : ', os.path.join(path, f))
                     # print(folder.size)
-                    self.cur_level += 1
                     folder.files_number += self.walking(os.path.join(path, f))
                     files_number += folder.files_number
+                    self.cur_level -= 1
 
             return files_number
         except PermissionError as e:
--- output.py
+++ output.py
@@ -2,7 +2,7 @@ def output(level_files_and_dirs):
     print('Name:  ', 'Size:  ', 'Time:  ')
     for f in level_files_and_dirs:
         print()
-        print(f.name, f.size, f.time)
+        print(f.name, f.level)
         try: print(f.files_number)
         except Exception:
             pass
